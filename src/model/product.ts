97f517a5d253dca4abffddbaa4d6189b66445df3
Carla Craparotta <carlacraparotta@outlook.com>
2023-08-10 23:22:21 +0200

    added databse and pocketbase sdk

---
+++ src/model/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    collectionId: string;
+    collectionName: string;
+    cost: number;
+    created: string;
+    description: string;
+    id: string;
+    img: string;
+    name: string;
+    tmb: string;
+    updated: string;
+}
--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,9 +1,37 @@
+import { Product } from "../../model/product";
+import { useState, useEffect } from "react";
+import { pb } from "../../pocketbase";
+
 export function ShopPage() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+      loadData()
+    
+    }, [])
+    
+
+    function loadData() {
+        pb.collection('products').getList<Product>()
+          .then(res => {
+            setProducts(res.items)
+          }) 
+    }
+
     return (
         <div>
             <h1 className="title">SHOP</h1>
 
-            contenuto qui...
+            {
+            products.map(p => {
+                return(
+                    <li key={p.id}>
+                        {p.name}
+                    </li>
+                )
+            })
+           }
         </div>
     )
 }
